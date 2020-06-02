59988952e08e5a617715d1edd9f1ba7acbcee4c0
SvetlanaPN <svetlana.peksheva@devexpress.com>
2020-06-02 17:47:35 +0300

    dispose adapter

--- CS/ExcelDataBaseHelper.cs
+++ CS/ExcelDataBaseHelper.cs
@@ -5,27 +5,25 @@ using System.Text;
 using System.IO;
 using System.Data.OleDb;
 using System.Data;
+using DevExpress.XtraEditors;
 
-namespace WindowsApplication1
-{
-    public class ExcelDataBaseHelper
-    {
-        public static object OpenFile(string fileName)
-        {
+namespace WindowsApplication1 {
+    public class ExcelDataBaseHelper {
+        public static object OpenFile(string fileName) {
             var fullFileName = string.Format("{0}\\{1}", Directory.GetCurrentDirectory(), fileName);
-            if (!File.Exists(fullFileName))
-            {
-                System.Windows.Forms.MessageBox.Show("File not found");
+            if(!File.Exists(fullFileName)) {
+                XtraMessageBox.Show("File not found");
                 return null;
             }
             var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fullFileName);
-            var adapter =  new OleDbDataAdapter("select * from [Sheet1$]", connectionString);
-            var ds = new DataSet();
-            string tableName = "excelData";
-            adapter.Fill(ds, tableName);
-            DataTable data = ds.Tables[tableName];
-            return data;
+            using(var adapter = new OleDbDataAdapter("select * from [Sheet1$]", connectionString)) {
+                var ds = new DataSet();
+                string tableName = "excelData";
+                adapter.Fill(ds, tableName);
+                DataTable data = ds.Tables[tableName];
+                return data;
+            }
         }
-        
+
     }
 }
