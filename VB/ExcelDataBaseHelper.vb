f7d61fe1a2f640ea395698b6e0cf7657aa4b5bc6
DevExpressExampleBot <fakeEmail>
2022-02-08 15:20:34 +0300

    Source auto update [skip ci]

--- VB/ExcelDataBaseHelper.vb
+++ VB/ExcelDataBaseHelper.vb
@@ -1,28 +1,27 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.IO
 Imports System.Data.OleDb
 Imports System.Data
+Imports DevExpress.XtraEditors
 
 Namespace WindowsApplication1
-	Public Class ExcelDataBaseHelper
-		Public Shared Function OpenFile(ByVal fileName As String) As Object
-			Dim fullFileName = String.Format("{0}\{1}", Directory.GetCurrentDirectory(), fileName)
-			If (Not File.Exists(fullFileName)) Then
-				System.Windows.Forms.MessageBox.Show("File not found")
-				Return Nothing
-			End If
-			Dim connectionString  As String = String.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fullFileName)
-			Dim adapter = New OleDbDataAdapter("select * from [Sheet1$]", connectionString)
-			Dim ds = New DataSet()
-			Dim tableName As String = "excelData"
-			adapter.Fill(ds, tableName)
-			Dim data As DataTable = ds.Tables(tableName)
-			Return data
-		End Function
 
-	End Class
+    Public Class ExcelDataBaseHelper
+
+        Public Shared Function OpenFile(ByVal fileName As String) As Object
+            Dim fullFileName = String.Format("{0}\{1}", Directory.GetCurrentDirectory(), fileName)
+            If Not File.Exists(fullFileName) Then
+                XtraMessageBox.Show("File not found")
+                Return Nothing
+            End If
+
+            Dim connectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fullFileName)
+            Using adapter = New OleDbDataAdapter("select * from [Sheet1$]", connectionString)
+                Dim ds = New DataSet()
+                Dim tableName As String = "excelData"
+                adapter.Fill(ds, tableName)
+                Dim data As DataTable = ds.Tables(tableName)
+                Return data
+            End Using
+        End Function
+    End Class
 End Namespace
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,19 +1,15 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-			gridControl1.DataSource = ExcelDataBaseHelper.OpenFile("test.xls")
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            gridControl1.DataSource = ExcelDataBaseHelper.OpenFile("test.xls")
+        End Sub
+    End Class
 End Namespace
